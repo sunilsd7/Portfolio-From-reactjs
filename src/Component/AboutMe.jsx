efcdfab0307fe47bfb7c14473d9a9e93439f56e6
sunilsd7 <sumildumre555@gmail.com>
2025-01-24 18:24:15 +0545

    Updated

--- src/Component/AboutMe.jsx
+++ src/Component/AboutMe.jsx
@@ -9,13 +9,13 @@ const AboutMe = () => {
       <div className='text-center font-bold md:text-4xl text-2xl mb-10 mt-10 flex justify-center '> <span className='flex border-4 px-6  py-4 space-x-4 rounded-lg'><span className=' tracking-widest  '>
         About Me</span> < FaUser className='' /></span></div>
       <div className='  md:p-5 md:w-[70%]  mx-auto '>
-        <div className='flex'>
+        <div className='md:flex md:item-center justify-center'>
         <p className=' '>I am a  developer based in Nawalparasi,Nepal. I am an Information Technology undergraduate from TU. I am very passionate about improving my coding skills & developing applications & websites. I build WebApps and Websites using HTML, CSS ,NOde js , React js. Working for myself to improve my skills. Love to work in team and build  clones. <p> Based on a project created by me or another one, sent by you, I can program the website to be fully functional and responsive.In case of any problems or the need for changes, I can introduce new functionalities and solutions.</p>
      
 
 
 </p>
-<img src={WebImage} alt="" />
+<img src={WebImage} alt=""  className='item-center'/>
 </div>
 <div>
   <img src="" alt="img" />
