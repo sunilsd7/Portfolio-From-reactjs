9d6cd0a480e46b107e3b76686b8c0ea39f65f0e9
sunilsd7 <sumildumre555@gmail.com>
2025-01-24 11:13:01 +0545

    Updated

--- src/Component/Navbar.jsx
+++ src/Component/Navbar.jsx
@@ -6,23 +6,23 @@ const Navbar = () => {
   return (
     <div className='  text-white overflow-hidden font-small'>
       <nav className='  flex'>
-        
-        <div className='   px-10  md:w-[50%] md:clip-right  py-6'>
-        <div  className='font-bold md:text-3xl  md:px-8'>SD SHARMA</div>
-        </div>
-       
-       
-        <div className='md:ms-auto md:bg-white md:w-[80%] clip-nav md:text-black font-medium text-2xl'>
-            <ul className='flex gap-x-10 justify-end mt-4 px-10 '>
-                <li> <a href="#aboutme" className='hover:text-blue'>About Me</a> </li>
-                <li><a href="#skills" className='hover:text-blue'>Skills</a></li>
-                <li><a href="#projects" className='hover:text-blue'>Projects</a></li>
 
-                <li><a href="#contactme" className='hover:text-blue' >Contact Me</a></li>
-                <li><a className='border-2 rounded-lg p-2 hover:text-blue flex' download="Sunil_CV.pdf" href='Sunil-Dumre-cv.pdf'>Download CV <IoMdDownload className='m-2 '/></a></li>
-            </ul>
+        <div className='   px-10  md:w-[50%] md:clip-right  py-6'>
+          <div className='font-bold md:text-3xl  md:px-8'>SD SHARMA</div>
         </div>
-       
+
+
+        <div className='md:ms-auto md:bg-white md:w-[90%] clip-nav md:text-black font-medium text-2xl'>
+          <ul className='flex gap-x-10 justify-end mt-4 px-10 '>
+            <li> <a href="#aboutme" className='hover:text-blue'>About</a> </li>
+            <li><a href="#skills" className='hover:text-blue'>Skills</a></li>
+            <li><a href="#projects" className='hover:text-blue'>Projects</a></li>
+
+            <li><a href="#contactme" className='hover:text-blue' >Contact</a></li>
+            <li><a className='border-2 rounded-lg p-2 hover:text-blue flex' download="Sunil_CV.pdf" href='Sunil-Dumre-cv.pdf'>Download CV <IoMdDownload className='m-2 ' /></a></li>
+          </ul>
+        </div>
+
       </nav>
     </div>
   )
