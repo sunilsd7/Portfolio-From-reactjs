75bd5ba6238a125d6cfea90300aebf97042f8047
sunilsd7 <sumildumre555@gmail.com>
2025-01-24 18:41:53 +0545

    Updated

--- src/Component/AboutMe.jsx
+++ src/Component/AboutMe.jsx
@@ -10,7 +10,7 @@ const AboutMe = () => {
         About Me</span> < FaUser className='' /></span></div>
       <div className='  md:p-5  '>
         <div className='md:flex md:items-center justify-center '>
-        <p className=' '>I am a  developer based in Nawalparasi,Nepal. I am an Information Technology undergraduate from TU. I am very passionate about improving my coding skills & developing applications & websites. I build WebApps and Websites using HTML, CSS ,Node js , React js. Working for myself to improve my skills. Love to work in team and build  clones. <p> Based on a project created by me or another one, sent by you, I can program the website to be fully functional and responsive.In case of any problems or the need for changes, I can introduce new functionalities and solutions.</p>
+        <p className='text-xl '>I am a  developer based in Nawalparasi,Nepal. I am an Information Technology undergraduate from TU. I am very passionate about improving my coding skills & developing applications & websites. I build WebApps and Websites using HTML, CSS ,Node js , React js. Working for myself to improve my skills. Love to work in team and build  clones. <p> Based on a project created by me or another one, sent by you, I can program the website to be fully functional and responsive.In case of any problems or the need for changes, I can introduce new functionalities and solutions.</p>
      
 
 
