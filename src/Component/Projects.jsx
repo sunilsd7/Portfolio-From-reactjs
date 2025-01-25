8213d339419fe67e79d81243f357d6ffb39c3625
sunilsd7 <sumildumre555@gmail.com>
2025-01-25 20:54:35 +0545

    Updated responsive for all screen

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -16,7 +16,7 @@ const Projects = () => {
       <div className="md:grid md:grid-cols-2 px-5 gap-16 mt-5">
         <div className='border-2 p-5 my-4 rounded-[1rem] '>
             <img src={Furniture} alt="" />
-            <p className='font-bold text-2xl mt-3'>Furniture website:demo project from react</p>
+            <p className='font-bold text-2xl mt-3'>Furniture Website: Demo project using react</p>
             <div className='space-x-6 mt-3 flex '>
             <button className='border-4 rounded-lg md:text-2xl px-2  bg-blue '><a href="https://github.com/sunilsd7/Furniture-website.git" target='_blank'>Github Repo</a></button>
             <button className='border-4  border-white rounded-lg md:text-2xl  hover:text-blue flex  items-center gap-2 p-2'><a href="https://furniturewebsitee.netlify.app/" target='_blank'> View Live </a><span className=''><FaEye/> </span></button></div>
@@ -25,7 +25,7 @@ const Projects = () => {
         
             <div className='border-2 p-5 my-4 rounded-[1rem]'>
             <img src={Medimitra} alt="img" />
-            <p className='font-bold text-2xl mt-3'>Medi-mitra: A smart Vaccination management System</p>
+            <p className='font-bold text-xl mt-3'>Medi-mitra: A smart Vaccination management System</p>
             <button className='border-4 rounded-lg text-2xl  bg-blue p-2 mt-2'><a href="https://github.com/sunilsd7/Medi-mitra.git" target='_blank'>Github Repo</a></button>
 
             </div>
