e929b7dde533a6e62a6a63d38a91904559814ad6
sunilsd7 <sumildumre555@gmail.com>
2025-01-25 21:07:14 +0545

    Updated responsive for all screen

--- src/Component/Footer.jsx
+++ src/Component/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <div>
-      <p className='text-center md:text-4xl text-l '>Sunil Dumre Shamra ©2025 WEB DEVELOPER</p>
+      <p className='text-center md:text-2xl text-l '>Sunil Dumre Shamra ©2025 WEB DEVELOPER</p>
     </div>
   )
 }
