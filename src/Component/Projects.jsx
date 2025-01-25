7d64beee835694ad933818a662ccd1afe444cce3
sunilsd7 <sumildumre555@gmail.com>
2025-01-25 20:52:31 +0545

    Updated responsive for all screen

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -7,7 +7,7 @@ import { FaEye } from "react-icons/fa";
 
 const Projects = () => {
   return (
-    <section className='px-'>
+    <section className='px-2'>
     <div id='projects'>
       <div className='text-center mx-auto font-bold text-4xl   p-2 '><span className=' border-4 rounded-lg p-2 px-5'>
         Projects </span>
@@ -18,15 +18,15 @@ const Projects = () => {
             <img src={Furniture} alt="" />
             <p className='font-bold text-2xl mt-3'>Furniture website:demo project from react</p>
             <div className='space-x-6 mt-3 flex '>
-            <button className='border-4 rounded-lg md:text-2xl px-2 hover:text-blue '><a href="https://github.com/sunilsd7/Furniture-website.git" target='_blank'>Github Repo</a></button>
-            <button className='border-4  border-white rounded-lg md:text-2xl  bg-blue flex  items-center gap-2 p-2'><a href="https://furniturewebsitee.netlify.app/" target='_blank'> View Live </a><span className=''><FaEye/> </span></button></div>
+            <button className='border-4 rounded-lg md:text-2xl px-2  bg-blue '><a href="https://github.com/sunilsd7/Furniture-website.git" target='_blank'>Github Repo</a></button>
+            <button className='border-4  border-white rounded-lg md:text-2xl  hover:text-blue flex  items-center gap-2 p-2'><a href="https://furniturewebsitee.netlify.app/" target='_blank'> View Live </a><span className=''><FaEye/> </span></button></div>
             
         </div>
         
             <div className='border-2 p-5 my-4 rounded-[1rem]'>
             <img src={Medimitra} alt="img" />
             <p className='font-bold text-2xl mt-3'>Medi-mitra: A smart Vaccination management System</p>
-            <button className='border-4 rounded-lg text-2xl px-2 hover:text-blue '><a href="https://github.com/sunilsd7/Medi-mitra.git" target='_blank'>Github Repo</a></button>
+            <button className='border-4 rounded-lg text-2xl  bg-blue p-2 mt-2'><a href="https://github.com/sunilsd7/Medi-mitra.git" target='_blank'>Github Repo</a></button>
 
             </div>
            
